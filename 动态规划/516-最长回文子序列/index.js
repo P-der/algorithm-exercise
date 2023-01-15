81cfae2d883ca33c172007bf3e2548a453025bfb
P-der <1198811801@qq.com>
2023-01-15 15:58:48 +0800

    leetcode: 516

--- 动态规划/516-最长回文子序列/index.js
+++ 动态规划/516-最长回文子序列/index.js
@@ -1,4 +1,5 @@
 // https://leetcode.cn/problems/longest-palindromic-subsequence/
+// DP table
 var longestPalindromeSubseq = function(s) {
     let n = s.length;
     let dp = new Array(n).fill(0).map(()=> new Array(n).fill(0))
@@ -15,4 +16,22 @@ var longestPalindromeSubseq = function(s) {
         }
     } 
     return dp[0][n-1]
+};
+// 压缩 DP table
+var longestPalindromeSubseq = function(s) {
+    let n = s.length;
+    let dp = new Array(n).fill(1)
+    for(let i = n-2; i >=0; i--) {
+        let pre = 0; // 每次循环中记录上一个的值
+        for(let j = i+1; j< n; j++) {
+            let temp = dp[j]; // 记录处理前的
+            if(s[i]=== s[j]) {
+                dp[j] = pre + 2
+            }else {
+                dp[j] = Math.max(dp[j], dp[j-1])
+            }
+            pre = temp; // 记录
+        }
+    } 
+    return dp[n-1]
 };
