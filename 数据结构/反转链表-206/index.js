eb1cae6d41951ba695d474442e2e6a2246c9c871
P-der <1198811801@qq.com>
2023-01-18 21:50:07 +0800

    leetcode: 206

---
+++ 数据结构/反转链表-206/index.js
@@ -0,0 +1,22 @@
+// https://leetcode.cn/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(!head || !head.next) {
+        return head
+    }
+    let last = reverseList(head.next);
+    head.next.next = head
+    head.next = null
+    return last
+};
