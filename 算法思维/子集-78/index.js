41a5f807331297d80d2f6fcd4655b085863d22e6
P-der <1198811801@qq.com>
2023-01-18 22:51:09 +0800

    leetcode: 78

--- 算法思维/子集-78/index.js
+++ 算法思维/子集-78/index.js
@@ -1,4 +1,5 @@
 // https://leetcode.cn/problems/subsets/
+// 数学归纳
 var subsets = function(nums) {
     if(!nums.length)  return [[]]
     let n = nums.pop();
@@ -8,4 +9,19 @@ var subsets = function(nums) {
         res.push([...res[i],n])
     }
     return res
+};
+// 回溯算法
+var subsets = function(nums) {
+    let res = []
+    let track = []
+    backtrack(nums, 0 , track)
+    return res
+    function backtrack(nums, start, track) {
+        res.push([...track])
+        for(let i = start; i < nums.length; i++) {
+            track.push(nums[i])
+            backtrack(nums, i+1, track)
+            track.pop()
+        }
+    }
 };
