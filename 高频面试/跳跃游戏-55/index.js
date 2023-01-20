dbdbc82f4305008437a8db009653ca0dd069cfde
P-der <1198811801@qq.com>
2023-01-20 22:59:41 +0800

    leetcode: 55

---
+++ 高频面试/跳跃游戏-55/index.js
@@ -0,0 +1,13 @@
+// https://leetcode.cn/problems/jump-game/
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let farthest = 0 
+    for(let i = 0; i<nums.length-1;i++) {
+        farthest= Math.max(farthest, i + nums[i])
+        if(farthest <= i) return false
+    }
+    return farthest >= nums.length -1
+};
